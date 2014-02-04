c2374dda1396047ad4e2aa96d54988e9834a354f
emoose <abc@cock.li>
2014-02-04 03:40:00 +0000

    page and pools list update

--- pools.php
+++ pools.php
@@ -5,7 +5,7 @@ $fedoracore['url'] = "http://core.fedoracoin.net/";
 $fedoracore['fee'] = 1;
 $fedoracore['verified'] = true;
 $fedoracore['type'] = "Proportional";
-$fedoracore['owner'] = "tipsfedora/invisibel";
+$fedoracore['owner'] = "tipsfedora/invisibel (TIPS developer)";
 
 $pcfil = array();
 $pcfil['name'] = "PCFiL";
@@ -29,13 +29,31 @@ $suchpool['url'] = "http://tips.suchpool.pw/";
 $suchpool['fee'] = 1;
 $suchpool['verified'] = false;
 $suchpool['type'] = "Proportional and VARDIFF";
-$suchpool['owner'] = "CryptoSiD";
+$suchpool['owner'] = "CryptoSiD and eth2";
+
+$coinpool = array();
+$coinpool['name'] = "Coin-Pool.org";
+$coinpool['url'] = "http://fedora.coin-pool.org/";
+$coinpool['fee'] = 1;
+$coinpool['verified'] = true;
+$coinpool['type'] = "Proportional and VARDIFF";
+$coinpool['owner'] = "speed-";
+
+$dedicated = array();
+$dedicated['name'] = "DedicatedPool.com";
+$dedicated['url'] = "http://tips.dedicatedpool.com/";
+$dedicated['fee'] = 2;
+$dedicated['verified'] = false;
+$dedicated['type'] = "Proportional?";
+$dedicated['owner'] = "binaryclock at #dedicatedpool on freenode";
 
 $pools = array();
 $pools[] = $fedoracore;
 $pools[] = $pcfil;
 $pools[] = $strips;
 $pools[] = $suchpool;
+$pools[] = $coinpool;
+$pools[] = $dedicated;
 
 shuffle($pools);
 header('Content-Type: application/json');
