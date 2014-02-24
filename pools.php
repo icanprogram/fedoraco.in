009b35fe34a67516ff14633297b5e365f76ea5aa
Ican Program <icanprogramfedoracoins@gmail.com>
2014-02-24 19:14:03 +0100

    Removed non-active pools from the list

--- pools.php
+++ pools.php
@@ -22,12 +22,9 @@ class Pool
 }
 
 $pools = array();
-$pools[] = new Pool("FedoraCore", "http://core.fedoracoin.net/", 1, true, "Proportional", "tipsfedora/invisibel", "mpos");
 $pools[] = new Pool("PCFiL", "http://fedora.pcfil.com/", 1, true, "Proportional", "PCFiL", "mpos");
 $pools[] = new Pool("ChickenStrips.net", "http://chickenstrips.net/", 0.5, true, "Proportional", "chisefu", "mpos");
-$pools[] = new Pool("SuchPool.pw", "http://tips.suchpool.pw/", 1, false, "Proportional and VARDIFF", "CryptoSiD", "mpos");
 $pools[] = new Pool("Coin-Pool.org", "http://fedora.coin-pool.org/", 1, true, "Proportional and VARDIFF", "speed-", "mpos");
-$pools[] = new Pool("DedicatedPool.com", "http://tips.dedicatedpool.com/", 2, false, "Proportional?", "binaryclock (#dedicatedpool@freenode)", "mpos");
 
 shuffle($pools);
 header('Content-Type: application/json');
